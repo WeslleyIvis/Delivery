454092319a66e68eb7acb99d8800d0d3deb3155c
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-10 10:41:56 -0300

    no impactful change

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,10 +3,10 @@ import './App.css';
 import FormLogin from './componentes/FormLogin';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './componentes/Main';
+import FormProduct from './componentes/FormProduct';
 
 function App() {
   const [token, setToken] = React.useState<String | null>(null);
-  sessionStorage.setItem('Authorization', '');
 
   function handleToken(tokenGenerate: string) {
     setToken(tokenGenerate);
@@ -19,7 +19,7 @@ function App() {
         {token && (
           <Route path={`/admIndex${token}`} element={<Main />} />
         )}
-        <Route path='/index' element={<Main />} />
+        <Route path='/index' element={<FormProduct />} />
       </Routes>
     </Router>
   );
--- my-app/src/componentes/Main.tsx
+++ my-app/src/componentes/Main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AsideADM from './AsideADM'
 
 const Main = () => {
   return (
     <div>
-      
+        <AsideADM />
     </div>
   )
 }
