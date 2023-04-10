5b1a4963ff7ed510bde546d79911ce97426fe5ff
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-09 23:59:23 -0300

    Auth form user and style form

--- my-app/src/componentes/FormLogin.tsx
+++ my-app/src/componentes/FormLogin.tsx
@@ -1,16 +1,64 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 
 const FormLogin = () => {
+  const [name, setName] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [status, setStatus] = React.useState('');
+
+  async function submit(event: SyntheticEvent) {
+    event.preventDefault();
+
+    await fetch('http://localhost:3333/auth/authenticate', {
+      method: 'POST',
+      body: JSON.stringify({email: name, password}),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then(data => valid(data))
+    .catch(err => console.log({error: err, }))
+  }
+
+  const valid = async (tokenData: {error: string, token: string}) => {
+    const {error, token} = tokenData;
+
+    if(token) {
+      return console.log({token: token});
+    }
+
+    if(error) {
+      return setStatus("Invalid name or password")
+    }else
+      return setStatus('Error conection db server')
+  }
+
   return (
     <div className="content-form-login">
-      <form>
+      <form onSubmit={submit}>
         <label htmlFor="name">Name</label>
-        <input type="text" name="email" id="name" placeholder='Login name'/>
+        <input 
+        type="text"
+        name="email"
+        id="name" 
+        placeholder='Login name' 
+        value={name} 
+        onChange={(event) => setName(event.target.value)}
+        />
         <label htmlFor="password">Password</label>
-        <input type="text" name="password" id="password" placeholder='Password'/>
-
+        <input 
+        type="text" 
+        name="password" 
+        id="password" 
+        placeholder='Password'
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        />
         <button type="submit">Sign In</button>
+        <p>{status}</p>
       </form>
+
+ 
     </div>
   )
 }
