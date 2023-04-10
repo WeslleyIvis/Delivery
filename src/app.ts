e2d06a12bcbc99e8732f2627aca20556be3975e0
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-09 23:58:58 -0300

    Port server node

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import app from './controllers/server';
 
-const PORT = process.env.PORT 
+const PORT = process.env.LOCAL_PORT || 3333;
 
 app.listen(PORT, () => {
     console.log(`On port: ${PORT}`)
