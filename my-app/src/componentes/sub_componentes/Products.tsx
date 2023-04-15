bbe9758e95ffec867f989303a076d8f517121e5f
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-15 04:05:15 -0300

    Feature: componente Products

--- my-app/src/componentes/sub_componentes/Products.tsx
+++ my-app/src/componentes/sub_componentes/Products.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { Product } from './CategoryProductNavSelection';
 
-const Products = () => {
+const Products = (props: {category: string}) => {
+  const [data, setData] = React.useState<Product[]>();
+  
+  async function filterCategory() {
+    await fetch('http://localhost:3333/project/product')
+    .then(r => r.json())
+    .then(value => setData(value))
+    .catch(err => console.log({error: err}));
+  }
+
+  React.useEffect(() => {
+    filterCategory()
+  }, [])
+
   return (
-    <div>
-      
-    </div>
+    <section>
+    <div className='products-content'>
+      {data && data ? 
+      data.filter(product => product.category === props.category)
+      .map((element, index) => {
+        return <div className='product-hover'>  
+          <div key={index} className='products-card'>
+            <img src={element.image} alt={element.name} />
+            <div>
+              <h3>{element.name}</h3>
+              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+              <p>{element.value}</p>
+              <button>
+                <img src={require("../images/bag.png")} alt="" /> Comprar</button>
+            </div>
+          </div>
+        </div>
+      }) : null}  
+    </div>  
+
+    </section>
   )
 }
 
