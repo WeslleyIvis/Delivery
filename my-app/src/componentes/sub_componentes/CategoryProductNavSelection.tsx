386fac0419d9cfddeabda077e9448c0d66eec4ca
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-15 04:02:57 -0300

    Controller Layout products

---
+++ my-app/src/componentes/sub_componentes/CategoryProductNavSelection.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Products from './Products';
+
+export interface Product {
+    _id: string;
+    name: string;
+    value: string;
+    category: string;
+    image: string;
+  }
+
+const CategoryProductNavSelection = (props: {category: string}) => {
+
+  return (
+    <section>      
+        <div className='category-section'>
+            <div>
+                <a href="/">Home - </a>
+                <span>{props.category}</span>
+                <h1>{props.category.toUpperCase()}</h1>
+            </div>
+        </div>
+
+        {props.category && props.category ? <Products category={props.category}/> : null}    
+    </section>
+  )
+}
+
+export default CategoryProductNavSelection
