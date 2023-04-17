2c60519c11b6fbb4772493366b14cb92a30c79f2
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-16 23:56:56 -0300

    Create products in main layout

--- my-app/src/componentes/sub_componentes/MainLayout.tsx
+++ my-app/src/componentes/sub_componentes/MainLayout.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-import Navbar from '../Navbar'
+import Products from './Products';
+
 
 const MainLayout = () => {
+    const [category, setCategory] = React.useState('Açai');
+
+    function handleCategory(props: React.SyntheticEvent) {
+        setCategory((props.target as HTMLLIElement).innerHTML);
+    }
+ 
   return (
     <div>
         <section className='slide'>
@@ -15,7 +22,18 @@ const MainLayout = () => {
                 <img src={require('../images/banner1.png')} alt="" />
             </div>
         </section>
-       
+
+        <section className='menu-content'>
+            <ul className='menu-selection'>
+                <li className='active' onClick={event => handleCategory(event)}>Açai</li>
+                <li onClick={event => handleCategory(event)}>Salgados</li>
+                <li onClick={event => handleCategory(event)}>Cafe</li>
+                <li onClick={event => handleCategory(event)}>Hamburguers</li>
+            </ul>
+            
+            <Products category={category}/>
+
+        </section>  
     </div>
   )
 }
