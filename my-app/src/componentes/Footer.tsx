ccfd1068f9e297e33b4122949db8363ee2f54014
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-17 02:50:53 -0300

    Footer Componente and style

--- my-app/src/componentes/Footer.tsx
+++ my-app/src/componentes/Footer.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-      
+    <footer className='footer'>
+      <div className='footer-logo'>
+        <img src="https://seeklogo.com/images/F/food-logo-59E5A73AFD-seeklogo.com.png" alt="logo"  />
+      </div>
+      <div className='footer-redes'>
+        <img src={require("./images/iconTwiter.png")} alt="" />
+        <img src={require("./images/iconFace.png")} alt="" />
+        <img src={require("./images/iconTwiter.png")} alt="" />
+      </div>
+      <div>
+        <p>@ Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      </div>
     </footer>
   )
 }
