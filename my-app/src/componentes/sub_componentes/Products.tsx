833a691708e11ebd281875d6da4bfd1ec0668362
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-21 02:52:33 -0300

    Feature: produts component with item limit

--- my-app/src/componentes/sub_componentes/Products.tsx
+++ my-app/src/componentes/sub_componentes/Products.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Product } from './CategoryProductNavSelection';
 
-const Products = (props: {category: string, display: string, amount: number} ) => {
+const Products = (props: { category: string, display?: string, amount?: number } = { category: '', display: 'products-content', amount: -1 } ) => {
   const [data, setData] = React.useState<Product[]>();
+  const [countAmount, setCountAmount] = React.useState(props.amount);
+  const [filterAmount, setFilterCount] = React.useState(0);
   
   async function filterCategory() {
     await fetch('http://localhost:3333/project/product')
     .then(r => r.json())
-    .then(value => setData(value))
+    .then(value => {
+      setData(value)
+    })
     .catch(err => console.log({error: err}));
   }
 
@@ -15,28 +19,36 @@ const Products = (props: {category: string, display: string, amount: number} ) =
     filterCategory()
   }, [])
 
+  React.useEffect(() => {
+    if (data) {
+      const count = data.filter(product => product.category === props.category).length;
+      setFilterCount(count);
+    }
+    setCountAmount(props.amount);
+  }, [data, props.category, props.amount])
+
   return (
     <section>
-    <div className={props.display}>
-      {data && data ? 
-      data.filter(product => product.category === props.category)
-      .slice(0, props.amount)
-      .map((element, index) => {
-        return <div className='product-hover' key={element.name + index}>  
-          <div className='products-card'>
-            <img src={element.image} alt={element.name} />
-            <div>
-              <h3>{element.name}</h3>
-              <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
-              <p>{element.value}</p>
-              <button>
-                <img src={require("../images/bag.png")} alt="" /> Comprar</button>
+      <div className={props.display}>
+        {data && data ? 
+        data.filter(product => product.category === props.category)
+        .slice(0, countAmount)
+        .map((element, index) => {
+          return <div className='product-hover' key={element.name + index}>  
+            <div className='products-card'>
+              <img src={element.image} alt={element.name} />
+              <div>
+                <h3>{element.name}</h3>
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+                <p>{element.value}</p>
+                <button>
+                  <img src={require("../images/bag.png")} alt="bag" /> Comprar</button>
+              </div>
             </div>
           </div>
-        </div>
-      }) : null}  
-    </div>  
-
+        }) : null}  
+      </div>  
+      {countAmount && countAmount < filterAmount ? <div onClick={() => setCountAmount(countAmount + 8)} className='products-add-items'></div> : null}
     </section>
   )
 }
