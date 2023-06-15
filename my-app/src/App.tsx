59f7cfad05c0dcd6c4833cd31cf1c32e3e8ee51a
WeslleyIvis <weslley_reive@hotmail.com>
2023-06-15 20:50:28 -0300

    search state

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,16 +6,21 @@ import Main from './componentes/Main';
 
 function App() {
   const [category, setCategory] = React.useState<string>('');
+  const [search, setSearch] = React.useState<string>('');
 
   const handleCategory = (r: string) => {
     setCategory(r);
   }
 
+  const handleSearch = (r: string) => {
+    setSearch(r);
+  }
+
   return (
     <div>
         <Header />
-        <Navbar handleCategory={handleCategory}/>
-        <Main category={category}/>
+        <Navbar handleCategory={handleCategory} handleSearch={handleSearch}/>
+        <Main category={category} name={search}/>
     </div>)
 }
 
